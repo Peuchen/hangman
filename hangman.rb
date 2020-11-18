c47db84a4d4a2be336aa5a9645b63a91b6ff4e96
peuchen <npeuchen@gmail.com>
2020-11-18 22:17:28 +0100

    Initiate hangman.rb with pseudocode

---
+++ hangman.rb
@@ -0,0 +1,16 @@
+#When a new game is started, load in the dictionary and randomly select a word between 5 and 12 characters long for the secret word
+
+#Display a counter so the player knows how many more incorrect guesses they have before the game ends
+
+#Display which correct letters have already been chosen (and their position in the word, e.g. _ r o g r a _ _ i n g) and which incorrect letters have already been chosen
+
+#Every turn, allow the player to make a guess of a letter (it should be case insensitive)
+
+#Update the display to reflect whether the letter was correct or incorrect
+#If out of guesses, the player should lose
+
+#AFTER DEVELOPING THE GAME
+
+#Instead of making a guess, the player should also have the option to save the game
+
+#When the program first loads, allow the player to open one of their saved games, which jumps exactly back to the saved moment
