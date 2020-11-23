050b5f92c4afe0c3056e2924018ba1ffe2f954f4
peuchen <npeuchen@gmail.com>
2020-11-23 18:06:59 +0100

    Refactor Game class

--- hangman.rb
+++ hangman.rb
@@ -1,8 +1,6 @@
-#When a new game is started, load in the dictionary and randomly select a word between 5 and 12 characters long for the secret word
 class Game
   def initialize
     $win = false
-
     @possible_words = []
     @incorrect_guesses = []
 
@@ -12,30 +10,30 @@ class Game
         @possible_words << word
       end
     end
-    @secret_word = @possible_words.shuffle.first
 
+    @secret_word = @possible_words.shuffle.first
     @placeholder = "-" * @secret_word.length
+
   end
 
   def play
     @strike = 0
     until $win
-      count(@strike)
+      display(@strike)
       feedback(guess)
       if @strike == 10
-        puts "You have lost the game. The secret word was #{@secret_word}."
+        puts "You have lost the game. The secret word was '#{@secret_word}'."
         break
       end
     end
-    puts "You have won. Congratulations!" if $win
-  end
-#Display a counter so the player knows how many more incorrect guesses they have before the game ends
-  def count(turn)
-    puts "********************\nYou have got #{10-@strike} guesses left."
-    puts "Secret word: #{@placeholder} (#{@placeholder.length} letters)"
+    puts "Secret word: #{@placeholder} (#{@placeholder.length} letters)\nYou have won, congratulations!" if $win
   end
 
-#Every turn, allow the player to make a guess of a letter (it should be case insensitive)
+  def display(strike)
+    puts "********************\nYou have got #{10-strike} guesses left."
+    puts "Secret word: #{@placeholder} (#{@placeholder.length} letters)"
+    puts "Incorrect: #{@incorrect_guesses.sort.join(" ")}"
+  end
 
   def guess
     puts "Please guess a letter."
@@ -47,25 +45,19 @@ class Game
     @guess
   end
 
-#Display which correct letters have already been chosen (and their position in the word, e.g. _ r o g r a _ _ i n g) and which incorrect letters have already been chosen
-
   def feedback(guess)
     if @secret_word.include?(guess)
-      puts "Your guess '#{guess}' is included in the secret word."
+      puts "'#{guess}' is included in the secret word."
       @secret_word.split("").each_with_index do |letter, idx|
         @placeholder[idx] = letter if letter == guess
       end
+      $win = true unless @placeholder.include?("-")
     else
+      puts "'#{guess}' is NOT included in the secret word."
       @incorrect_guesses << guess
       @strike += 1
     end
-    puts "Incorrect: #{@incorrect_guesses.sort.join(" ")}"
-
-    $win = true unless @placeholder.include?("-")
   end
-
-#Update the display to reflect whether the letter was correct or incorrect
-#If out of guesses, the player should lose
 
 end
 
