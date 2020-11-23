83cdfd675f1a7d5fad8ab381d09c2c89e535dca9
peuchen <npeuchen@gmail.com>
2020-11-23 19:13:44 +0100

    Add separate create_word_list method and refactor other methods

--- hangman.rb
+++ hangman.rb
@@ -1,48 +1,45 @@
 class Game
   def initialize
-    $win = false
-    @possible_words = []
     @incorrect_guesses = []
+    @strike = 0
 
-    @dictionary = File.readlines('5desk.txt')
-    @dictionary.each do |word| word.strip!
+    @secret_word = create_word_list.shuffle.first
+    @placeholder = "-" * @secret_word.length
+  end
+
+  def create_word_list
+    possible_words = []
+
+    dictionary = File.readlines('5desk.txt')
+    dictionary.each do |word| word.strip!
       if word.length > 4 && word.length < 13 && word == word.downcase
-        @possible_words << word
+        possible_words << word
       end
     end
-
-    @secret_word = @possible_words.shuffle.first
-    @placeholder = "-" * @secret_word.length
-
+    possible_words
   end
 
   def play
-    @strike = 0
-    until $win
-      display(@strike)
+    while @placeholder.include?("-") && @strike < 10
+      display
       feedback(guess)
-      if @strike == 10
-        puts "You have lost the game. The secret word was '#{@secret_word}'."
-        break
-      end
     end
-    puts "Secret word: #{@placeholder} (#{@placeholder.length} letters)\nYou have won, congratulations!" if $win
   end
 
-  def display(strike)
-    puts "********************\nYou have got #{10-strike} guesses left."
+  def display
+    puts "********************\nYou have got #{10-@strike} guesses left."
     puts "Secret word: #{@placeholder} (#{@placeholder.length} letters)"
-    puts "Incorrect: #{@incorrect_guesses.sort.join(" ")}"
+    puts "Incorrect: #{@incorrect_guesses.sort.join(' ')}"
   end
 
   def guess
     puts "Please guess a letter."
-    @guess = gets.chomp.downcase
-    until @guess.match(/[a-z]/) && @guess.length == 1
+    input = gets.chomp.downcase
+    until input.match(/[a-z]/) && input.length == 1
       puts "You have not chosen 1 letter from the alphabet. Please try again."
-      @guess = gets.chomp.downcase
+      input = gets.chomp.downcase
     end
-    @guess
+    input
   end
 
   def feedback(guess)
@@ -51,11 +48,14 @@ class Game
       @secret_word.split("").each_with_index do |letter, idx|
         @placeholder[idx] = letter if letter == guess
       end
-      $win = true unless @placeholder.include?("-")
+      puts "Secret word: #{@placeholder} (#{@placeholder.length} letters)\nYou have won, congratulations!" unless @placeholder.include?("-")
     else
       puts "'#{guess}' is NOT included in the secret word."
       @incorrect_guesses << guess
       @strike += 1
+      if @strike == 10
+        puts "You have lost the game. The secret word was '#{@secret_word}'."
+      end
     end
   end
 
